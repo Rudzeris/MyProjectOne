5e9af98c33626abcbdcb45b050a6757b8c76c482
Rudzeris <ruzil1atangulov@gmail.com>
2022-04-04 21:10:42 +0500

    #3 Добавил кнопку в экзаменах, наследовал html файлы

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,7 @@ app_name = 'core'
 
 urlpatterns = [
     path('', core.views.IndexView.as_view(), name='index'),
+    path('admin/', core.views.IndexView.as_view(), name='admin'),
     path('list/', core.views.Student_list.as_view(), name='Student_list'),
     path('list/<str:pk>/', core.views.Exam_list.as_view(), name='Exam_list'),
 ]
